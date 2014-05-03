b21c4691bfc6d005af16f393a27de7d1001de46f
Tim Cumming <timcumming123@gmail.com>
2014-05-03 15:33:21 +0100

    Version change to non alpha.

--- config.py
+++ config.py
@@ -18,7 +18,7 @@
 # Author: Tim Cumming aka Elusive One
 # Created: 01/04/14
 
-version = '0.0.1-alpha'
+version = '0.0.1'
 headers = {'User-Agent': ('Nett/%s +https://github.com/EluOne/Nett' % version)}
 
 # System db id numbers
